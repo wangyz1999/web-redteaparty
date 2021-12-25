1ef2eb88867e22fceb5da57eb017d205f60d3c96
wangyz1999 <yunzhewa@usc.edu>
2021-12-25 14:41:27 -0500

    more style

--- src/components/advantage.js
+++ src/components/advantage.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 
 const Advantage = ({dscpt, children}) => {
   return (
-    <div className='advantage border-2 p-4 max-w-2xl'>
+    <div className='h-80 w-60 rounded-3xl shadow-xl bg-lime-300 ml-auto mr-auto px-5 py-8 grid content-between'>
       {children}
       <h3 className='flex justify-center text-3xl'>{dscpt}</h3>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Advantage from '../components/advantage'
 import Posts from '../components/posts'
 import Record from '../components/record'
@@ -9,42 +10,50 @@ import "./index.css"
 
 const IndexPage = () => {
   return (
-    <div>
-      <main>
-        <div className='header flex flex-col justify-center items-center h-screen text-left'>
-          <div className='space-y-6'>
-            <h1 className='text-8xl font-chi'>红茶会</h1>
-            <p className='text-left text-2xl font-noto'>1.18.X 原版多人生存 | 高度自由 | 娱乐养老 | 精英怪</p>
-            <p className='text-left text-2xl font-noto'>QQ群: 170912922</p>
+    <div className='text-neutral-700 bg-stone-50'>
+      <Parallax pages={3} style={{ top: '0', left: '0' }}>
+        <ParallaxLayer offset={0} speed={2}>
+          <main>
+            <div className='header flex flex-col justify-center items-center h-screen text-left'>
+              <div className='space-y-6 px-4'>
+                <h1 className='text-8xl lg:text-xxl font-chi'>红茶会</h1>
+                <p className='text-left text-2xl lg:text-4xl font-noto'>1.18.X 原版多人生存 | 高度自由 | 娱乐养老 | 精英怪</p>
+                <p className='text-left text-2xl lg:text-4xl font-noto'>QQ群: 170912922</p>
+              </div>
+            </div>
+          </main>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={2}>
+          <div className='online flex justify-center items-center h-screen space-x-3'>
+            <p className='text-3xl'>服务器当前在线</p>
+            <p className='text-2xl'>-</p>
+            <p className='text-3xl'>人</p>
           </div>
-        </div>
-      </main>
-      <div className='online flex justify-center items-center h-screen space-x-3'>
-        <p className='text-3xl'>服务器当前在线</p>
-        <p className='text-2xl'>-</p>
-        <p className='text-3xl'>人</p>
-        </div>
-      <div className='sec-adv h-screen'>
-        <h2 className='text-8xl font-noto flex justify-center'>为什么选择我们</h2>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20 content-auto mt-20 ml-20 mr-20'>
-          <Advantage dscpt="专业"><p>采用腾讯云VPS，专业主机Linux系统，完全自主搭建。10兆网络供您稳定低延迟</p></Advantage>
-          <Advantage dscpt="安全"><p>圈地系统供您自定义领地规则，如遭到破坏可用CoreProtect针对个人回档</p></Advantage>
-          <Advantage dscpt="自由"><p>没有严格建造限制，可在主城边上造火柴屋，允许红石（前提是不卡服）</p></Advantage>
-          <Advantage dscpt="活跃"><p>游戏只是个幌子，构建网友交流群才是</p></Advantage>
-        </div>
-      </div>
-      <div className='sec-records mt-60'>
-        <h2>记录你的生存瞬间</h2>
-        <Record dscpt="卫星图"><p>asdasd</p></Record>
-        <Record dscpt="服务器摄影"><p>asdasd</p></Record>
-        <Record dscpt="街景地图"><p>asdasd</p></Record>
-      </div>
-      <div className='sec-posts mt-60'>
-        <Posts dscpt="新手指南"><p>asdasd</p></Posts>
-        <Posts dscpt="更新日志"><p>asdasd</p></Posts>
-        <Posts dscpt="历来活动"><p>asdasd</p></Posts>
-        <Posts dscpt="禁封记录"><p>asdasd</p></Posts>
-      </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} speed={2}>
+          <div className='sec-adv mt-40'>
+            <h2 className='text-4xl md:text-6xl font-noto flex justify-center'>为什么选择我们</h2>
+            <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 content-center mt-20 xl:mt-40 2xl:mx-56 xl:mx-40 lg:mx-20 md:mx-10 gap-y-20 gap-x-10'>
+              <Advantage dscpt="专业"><p>采用腾讯云VPS，专业主机Linux系统，完全自主搭建。10兆网络供您稳定低延迟</p></Advantage>
+              <Advantage dscpt="安全"><p>圈地系统供您自定义领地规则，如遭到破坏可用CoreProtect针对个人回档</p></Advantage>
+              <Advantage dscpt="自由"><p>没有严格建造限制，可在主城边上造火柴屋，允许红石（前提是不卡服）</p></Advantage>
+              <Advantage dscpt="活跃"><p>游戏只是个幌子，构建网友交流群才是</p></Advantage>
+            </div>
+          </div>
+          <div className='sec-records h-screen mt-40'>
+            <h2 className='text-6xl font-noto flex justify-center'>记录你的生存瞬间</h2>
+            <Record dscpt="卫星图"><p>asdasd</p></Record>
+            <Record dscpt="服务器摄影"><p>asdasd</p></Record>
+            <Record dscpt="街景地图"><p>asdasd</p></Record>
+          </div>
+          <div className='sec-posts h-screen mt-40'>
+            <Posts dscpt="新手指南"><p>asdasd</p></Posts>
+            <Posts dscpt="更新日志"><p>asdasd</p></Posts>
+            <Posts dscpt="历来活动"><p>asdasd</p></Posts>
+            <Posts dscpt="禁封记录"><p>asdasd</p></Posts>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   )
 }
